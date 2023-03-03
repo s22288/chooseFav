0c36f26fa108007c5863a9c86a2c931ea24bcc67
„Michal <s22288@pjwstk.edu.pl>
2023-03-03 18:35:10 +0100

    add empty project

---
+++ src/test/java/com/example/chooseFav/ChooseFavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chooseFav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooseFavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
